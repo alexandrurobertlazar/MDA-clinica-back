5722c2d4757e76afe777a3af91ec897cbdd9ce53
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-14 09:01:42 +0000

    Added DB connection

--- app/index.js
+++ app/index.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// DB Config
+//Set up default mongoose connection
+const mongoDB = 'mongodb://127.0.0.1/mda-clinica';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+//Get the default connection
+const db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// Server config
 const app = express();
 const port = 3000;
 
