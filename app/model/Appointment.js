7c820fde6856da58b73f8a8ea384c0505d9b47c9
Raul F. Sanchez Falcon <raulsf10@gmail.com>
2022-03-17 21:02:33 +0000

    Busqueda de citas por paciente y especialista

--- app/model/Appointment.js
+++ app/model/Appointment.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 
+
 //esperar al CRUD de usuarios para cambiar las String de paciente 
 //y especialista por ids de usuarios del sistema
 const AppointmentSchema = new Schema({
