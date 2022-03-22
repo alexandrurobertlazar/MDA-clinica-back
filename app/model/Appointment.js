b2948587925a665739442f636b45285469166a33
RaulFSF <raulsf10@gmail.com>
2022-03-22 19:07:10 +0000

    HU-6 validaciones

--- app/model/Appointment.js
+++ app/model/Appointment.js
@@ -1,10 +1,41 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const UserController = require('../controller/UserController');
 
+const validateDate = function(date){
+    let today = new Date(Date.now());
+    if(Date.parse(date) <= Date.parse(today.toISOString())){
+        return false;
+    } else{
+        return true;
+    }
+}
 
-//esperar al CRUD de usuarios para cambiar las String de paciente 
-//y especialista por ids de usuarios del sistema
+const validatePacient = async function(pacient){
+    const pacientValidate = await UserController.getUserById(pacient);
+    if(pacientValidate){
+        if(pacientValidate.role === "paciente" ){
+            return true;
+        } 
+        return false;
+    } else{
+        return false;
+    }
+}
+
+const validateEspecialist = async function(especialist){
+    const especialistValidate = await UserController.getUserById(especialist);
+    if(especialistValidate){
+        if(especialistValidate.role === "especialista" ){
+            return true;
+        } 
+        return false;
+    } else{
+        return false;
+    }
+}
+
 const AppointmentSchema = new Schema({
     title: {
         type : String,
@@ -12,11 +43,23 @@ const AppointmentSchema = new Schema({
             "Analitica",
             "Enfermeria",
             "Cita con medico"
-        ]
+        ],
+        required: "El tipo de cita es obligatorio"
     },
-    pacient: String,
-    especialist: String,
-    date: Date,
+    pacient: {
+        type: String,
+        required: "El paciente es obligatorio",
+        validate: [validatePacient, "No existe este paciente"]
+    },
+    especialist: {
+        type: String,
+        required: "El especialista es obligatorio",
+        validate: [validateEspecialist, "No existe este especialista"]
+    },
+    date: {
+        type : Date,
+        validate: [validateDate, "Fecha incorrecta"]
+    },
     desc: {
         type : String,
         trim: true,
