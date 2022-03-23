0a8a85a6a975cbd5782de73fa4ffb561dff1f743
RaulFSF <raulsf10@gmail.com>
2022-03-23 18:31:42 +0000

    HU-6 arreglos

--- app/controller/AppointmentController.js
+++ app/controller/AppointmentController.js
@@ -5,76 +5,98 @@ const mongoose = require('mongoose');
 const { appointmentHelper } = require('../helper/AppointmentHelper');
 
 async function getAppointmentById(id){
-    let o_id = mongoose.Types.ObjectId(id);
-    let filter = {"_id": o_id};
-    const appointment = await Appointment.findOne(filter);
-    return appointment;
+    try {
+        let o_id = mongoose.Types.ObjectId(id);
+        let filter = {"_id": o_id};
+        const appointment = await Appointment.findOne(filter);
+        return appointment;   
+    } catch (error) {
+        return false;
+    }
 }
 
 async function getAllAppointments(){
-    const appointment_list = await Appointment.find();
-    var apps = [];
-    appointment_list.forEach(app =>{
-        apps.push(appointmentHelper(app));
-    })
-    return apps;
+    try {
+        const appointment_list = await Appointment.find();
+        var apps = [];
+        appointment_list.forEach(app =>{
+            apps.push(appointmentHelper(app));
+        })
+        return apps;   
+    } catch (error) {
+        return false;
+    }
 }
 
 async function getAllAppointmentsByPacId(id){
-    let o_id = mongoose.Types.ObjectId(id);
-    let filter = {"pacient": o_id};
+    try {
+        let o_id = mongoose.Types.ObjectId(id);
+        let filter = {"pacient": o_id};
 
-    const appointment_list = await Appointment.find(filter);
-    
-    var apps = [];
-    appointment_list.forEach(app =>{
-        apps.push(appointmentHelper(app));
-    })
-    return apps;
+        const appointment_list = await Appointment.find(filter);
+        
+        var apps = [];
+        appointment_list.forEach(app =>{
+            apps.push(appointmentHelper(app));
+        })
+        return apps;
+    } catch (error) {
+        return false;
+    }
 }
 
 async function getAllAppointmentsByEspId(id){
-    let o_id = mongoose.Types.ObjectId(id);
-    let filter = {"especialist": o_id};
-
-    const appointment_list = await Appointment.find(filter);
-    var apps = [];
-    appointment_list.forEach(app =>{
-        apps.push(appointmentHelper(app));
-    })
-    return apps;
+    try {
+        let o_id = mongoose.Types.ObjectId(id);
+        let filter = {"especialist": o_id};
+    
+        const appointment_list = await Appointment.find(filter);
+        var apps = [];
+        appointment_list.forEach(app =>{
+            apps.push(appointmentHelper(app));
+        })
+        return apps;   
+    } catch (error) {
+        return false;
+    }
 }
 
 //PROBLEMA: con la fecha que se pone en la base de datos
 async function createAppointment(body){
-
-    const req = new Appointment(body);
-    req['date'] = new Date(body.date);
-
-    let validationError = req.validateSync();
-    if (validationError){
-        return {"validationError" : validationError};
+    try {
+        const req = new Appointment(body);
+        req['date'] = new Date(body.date);
+    
+        let validationError = req.validateSync();
+        if (validationError){
+            return {"validationError" : validationError};
+        }
+    
+        let id = await req.save();
+        const createdAppointment = await Appointment.findById({_id: String(id['_id'])}).exec();
+        return appointmentHelper(createdAppointment);
+    } catch (error) {
+        return false;
     }
-
-    let id = await req.save();
-    const createdAppointment = await Appointment.findById({_id: String(id['_id'])}).exec();
-    return appointmentHelper(createdAppointment);
 }
 
 async function updateAppointment(body, id){
-    
-    let o_id= mongoose.Types.ObjectId(id.trim());
+    try {
+        let o_id= mongoose.Types.ObjectId(id.trim());
 
-    const req = new Appointment({
-        _id: o_id,
-        title: body.title,
-        pacient: body.pacient,
-        especialist: body.especialist,
-        date: body.date,
-        desc: body.desc
-    });
-    let updatedAppointment = await Appointment.findOneAndUpdate({_id: o_id}, req, {new: true});
-    return appointmentHelper(updatedAppointment);
+        const req = new Appointment({
+            _id: o_id,
+            title: body.title,
+            pacient: body.pacient,
+            especialist: body.especialist,
+            date: body.date,
+            desc: body.desc
+        });
+        let updatedAppointment = await Appointment.findOneAndUpdate({_id: o_id}, req, {new: true});
+        return appointmentHelper(updatedAppointment);   
+    } catch (error) {
+        return false;
+    }
 }
 
 async function deleteAppointment(id){
@@ -83,7 +105,6 @@ async function deleteAppointment(id){
         let remove = await Appointment.findByIdAndDelete(o_id);
         return (remove !== null);
     } catch (error){
-        console.log(error);
         return false;   
     }
 }
